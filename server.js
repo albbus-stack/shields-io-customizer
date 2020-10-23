706938f03ca1001aff215c273a4cf26364306545
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2020-10-23 20:11:51 +0200

    Changed to ejs view engine

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+const path = require('path')
+const express = require('express'),
+    app = express()
 
-app.use("/static", express.static('./static/'));
+app.set('view engine', 'ejs');
 
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.set('views', [
+    path.join(__dirname, '/views'),
+]);
+
+function generateJSON(label, message) {
+    var jsonData = {
+        'schemaVersion': 1,
+        'label': label,
+        'message': message
+    };
+    return JSON.stringify(jsonData)
+}
+
+app.get('/', function(req, res) {
+    return res.render('index', {})
 });
 
-app.use('/', router);
+app.get('/getdata', function(req, res) {
+    var lab = req.query.lab
+    var mes = req.query.mes
+    res.send(generateJSON(lab, mes))
+});
+
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => console.log(`Node server listening on port ${app.get('port')}!`));
