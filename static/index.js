88354e1767ecaa90b60e0458c5f6f0cde74d6606
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2020-10-26 19:15:44 +0100

    Added dynamic badges section

--- static/index.js
+++ static/index.js
@@ -9,7 +9,19 @@ var app = new Vue({
         logoCol: "",
         logoWid: "",
         labelCol: "",
-        link: ""
+        link: "",
+        lab1: "",
+        mes1: "",
+        col1: "",
+        sty1: "",
+        logo1: "",
+        logoCol1: "",
+        logoWid1: "",
+        labelCol1: "",
+        link1: "",
+        type: "",
+        user: "",
+        repo: ""
     },
     methods: {
         copyMD: function() {
@@ -111,6 +123,119 @@ var app = new Vue({
                     window.URL.revokeObjectURL(url)
                 })
                 .catch(() => console.log('Download failed!'))
+        },
+        copyMD1: function() {
+            if (this.link != "") {
+                let md = '[!['.concat(this.lab, '](', this.URL, ')](', 'https://', this.link, ')')
+                if (window.clipboardData && window.clipboardData.setData) {
+                    return clipboardData.setData("Text", md)
+                } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                    var textarea = document.createElement("textarea")
+                    textarea.textContent = md
+                    textarea.style.position = "fixed"
+                    document.body.appendChild(textarea)
+                    textarea.select()
+                    try {
+                        return document.execCommand("copy")
+                    } catch (ex) {
+                        console.warn("Copy to clipboard failed.", ex)
+                        return false
+                    } finally {
+                        var notification = document.createElement("div")
+                        notification.className = "notification is-primary img is-size-6"
+                        notification.innerHTML = "Badge copied in .md format"
+                        notification.style.animation = "fade 2s ease-in 1s"
+                        document.body.appendChild(notification)
+                        setTimeout(function() {
+                            document.body.removeChild(notification)
+                        }, 3000)
+                        document.body.removeChild(textarea)
+                    }
+                }
+            } else {
+                let md = '!['.concat(this.lab, '](', this.URL, ')')
+                if (window.clipboardData && window.clipboardData.setData) {
+                    return clipboardData.setData("Text", md)
+                } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                    var textarea = document.createElement("textarea")
+                    textarea.textContent = md
+                    textarea.style.position = "fixed"
+                    document.body.appendChild(textarea)
+                    textarea.select()
+                    try {
+                        return document.execCommand("copy")
+                    } catch (ex) {
+                        console.warn("Copy to clipboard failed.", ex)
+                        return false
+                    } finally {
+                        var notification = document.createElement("div")
+                        notification.className = "notification is-primary img is-size-6"
+                        notification.innerHTML = "Badge copied in .md format"
+                        notification.style.animation = "fade 2s ease-in 1s"
+                        document.body.appendChild(notification)
+                        setTimeout(function() {
+                            document.body.removeChild(notification)
+                        }, 3000)
+                        document.body.removeChild(textarea)
+                    }
+                }
+            }
+        },
+        copyHTML1: function() {
+            let html = '<img src="'.concat(this.URL, '"', '>')
+            if (window.clipboardData && window.clipboardData.setData) {
+                return clipboardData.setData("Text", html)
+            } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+                var textarea = document.createElement("textarea")
+                textarea.textContent = html
+                textarea.style.position = "fixed"
+                document.body.appendChild(textarea)
+                textarea.select()
+                try {
+                    return document.execCommand("copy")
+                } catch (ex) {
+                    console.warn("Copy to clipboard failed.", ex)
+                    return false
+                } finally {
+                    var notification = document.createElement("div")
+                    notification.className = "notification is-primary img is-size-6"
+                    notification.innerHTML = "Badge copied in html format"
+                    notification.style.animation = "fade 2s ease-in 1s"
+                    document.body.appendChild(notification)
+                    setTimeout(function() {
+                        document.body.removeChild(notification)
+                    }, 3000)
+                    document.body.removeChild(textarea)
+                }
+            }
+        },
+        downloadSVG1: function() {
+            fetch('https://cors-anywhere.herokuapp.com/' + this.URL)
+                .then(resp => resp.blob())
+                .then(blob => {
+                    const url = window.URL.createObjectURL(blob)
+                    const a = document.createElement('a')
+                    a.style.display = 'none'
+                    a.href = url
+                    a.download = 'badge'.concat(this.label ? '-' + this.label : '', '.svg')
+                    document.body.appendChild(a)
+                    a.click()
+                    window.URL.revokeObjectURL(url)
+                })
+                .catch(() => console.log('Download failed!'))
+        },
+        resetInputs: function() {
+            this.lab1 = ""
+            this.mes1 = ""
+            this.col1 = ""
+            this.sty1 = ""
+            this.logo1 = ""
+            this.logoCol1 = ""
+            this.logoWid1 = ""
+            this.labelCol1 = ""
+            this.link1 = ""
+            this.user = ""
+            this.repo = ""
         }
     },
     computed: {
